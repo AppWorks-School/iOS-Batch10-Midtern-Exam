eb851d7e5c189657d692ab7940d3c16a71d3584e
Luke <wuchiwei17@gmail.com>
2019-08-12 15:36:44 +0800

    Init commit

---
+++ Medium-Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Medium-Playground
+//
+//  Created by WU CHIH WEI on 2019/8/12.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
